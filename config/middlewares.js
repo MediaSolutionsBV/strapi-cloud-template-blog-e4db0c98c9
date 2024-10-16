79d862c2cf5ba65fa72b4af55369f7cdf8125d5d
WillemvanEsch <119608969+WillemvanEsch@users.noreply.github.com>
2024-10-16 14:20:30 +0200

    met policy change

--- config/middlewares.js
+++ config/middlewares.js
@@ -7,7 +7,16 @@ module.exports = [
       contentSecurityPolicy: {
         useDefaults: true,
         directives: {
-          'script-src': ['https://cdn.ckeditor.com']
+          'script-src': [
+            "'self'", // Dit staat scripts toe van hetzelfde domein als je Strapi-app
+            'https://cdn.ckeditor.com', // CKEditor bron
+            'https://sublime-deer-8d96a70d84.strapiapp.com' // Voeg het domein toe waar je script vandaan komt
+          ],
+          'script-src-elem': [
+            "'self'",
+            'https://cdn.ckeditor.com',
+            'https://sublime-deer-8d96a70d84.strapiapp.com'
+          ],
         },
       },
     },
